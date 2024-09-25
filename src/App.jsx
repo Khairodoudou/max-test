05be806a55693f7290cfda2b669d2270c35d5abc
faroukq1 <faroukq1@gmail.com>
2024-09-25 18:59:09 +0100

    deploy commands

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,38 @@
-import NavBar from "./Components/NavBar"
-import Hero from "./Hero"
-import Hero2 from "./Hero2"
-import Section from "./Section"
-import Vedioo from "./Vedioo"
-import Bodyy from "./Bodyy"
-import Section3 from "./Section3"
-import Section4 from "./Section4"
-import Section2 from "./Section2"
-import Galerie from "./Galerie"
-import Sectionend from "./Sectionend"
-import Footer from "./Components/Footer"
+import NavBar from "./Components/NavBar";
+import Hero from "./Hero";
+import Hero2 from "./Hero2";
+import Section from "./Section";
+import Vedioo from "./Vedioo";
+import Bodyy from "./Bodyy";
+import Section3 from "./Section3";
+import Section4 from "./Section4";
+import Section2 from "./Section2";
+import Galerie from "./Galerie";
+import Sectionend from "./Sectionend";
+import Footer from "./Components/Footer";
 import { Element } from "react-scroll";
-
 
 export default function App() {
   return (
     <div>
-      <NavBar/>
-      <Hero/>
-      <Hero2/>
+      <NavBar />
+      <Hero />
+      <Hero2 />
       <Section />
       <Vedioo />
-      <Bodyy/>
+      <Bodyy />
       <Element name="Section3">
-        <Section3/>
+        <Section3 />
       </Element>
-
       <Element name="Section4">
         <Section4 />
       </Element>
-
       <Element name="Galerie">
-        <Galerie/>
+        <Galerie />
       </Element>
-      
-      
       <Sectionend />
       <Section2 />
       <Footer />
-
     </div>
-  )
+  );
 }
